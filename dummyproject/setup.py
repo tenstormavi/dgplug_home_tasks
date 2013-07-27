c41d4816f9638476343c48ac8dd9d615cee15ee9
Avinash Kumar Dasoundhi <avi.avinash3008@gmail.com>
2013-07-28 00:02:17 +0530

    Code

--- dummyproject/setup.py
+++ dummyproject/setup.py
@@ -1,36 +1,27 @@
 #!/usr/bin/env python
-"""avi_shell project"""
+"""avi_shellv1 project"""
+from setuptools import find_packages, setup
 
-requirements = []
-
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    requirements.append('setuptools-0.9.7')
-
-try:
-    import requests
-except ImportError:
-    requirements.append('requests')
-
-try:
-    from cmd2 import Cmd
-except ImportError:
-    requirements.append('cmd2')
-
-setup(name = 'avi_shell',
-    version = '1.0',
+setup(name = 'avi_shellv1',
+    version = '2.0',
     description = "tenstormavi's shell",
     long_description = "A Test Module",
     platforms = ["Linux"],
-    author = "tenstormavi",
-    author_email = "avi.avinash3008@gmail.com",
-    url = "https://github.com/tenstormavi",
+    author="Avinash Kumar Dasoundhi",
+    author_email="avi.avinash3008@gmail.com",
+    url="https://github.com/tenstormavi",
     license = "MIT",
-    packages = find_packages(),
-    install_requires=requirements,
-    scripts=['avi_shell'],
-    data_files=[('/usr/bin',['avi_shell']),
-    ('/usr/share/avi_shell', ['README.rst'])
-    ]
-    )
+    install_requires=[
+        "setuptools",
+        "requests",
+        "cmd2",
+        "pyparsing",
+    ],
+    dependency_links=[
+        "https://pypi.python.org/packages/source/r/requests/requests-1.2.3.tar.gz",
+        "https://pypi.python.org/pypi/cmd2/0.6.5.1#downloads",
+        "https://pypi.python.org/pypi/pyparsing/1.5.7#downloads",
+    ],
+    include_package_data=True,
+    packages=find_packages()
+)
